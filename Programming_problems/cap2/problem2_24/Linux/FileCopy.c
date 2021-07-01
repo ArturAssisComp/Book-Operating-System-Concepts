27e865ef4db67f1636019a4f1b9ff850cc4f82f4
ArturAssisComp <arturassiscomp@gmail.com>
2021-07-01 12:29:40 -0300

    Implement copy_from_to function for UNIX system.

--- Programming_problems/cap2/problem2_24/Linux/FileCopy.c
+++ Programming_problems/cap2/problem2_24/Linux/FileCopy.c
@@ -16,10 +16,20 @@
 #include <errno.h>
 #include <stdbool.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 //Macros:
 #define NON_UNIX_STD_ERROR_NO -1
-#define MAX_BUFFER_SZ 300
+#define MAX_BUFFER_SZ 350
+#define INVALID_FD -1
+#define READ_FAILED -1
+#define WRITE_FAILED -1
+
+//Data types:
+typedef unsigned char byte;
 
 //Declarations of the functions:
 bool copy_from_to(char source_file_name[], char destination_file_name[]);
@@ -54,7 +64,7 @@ int main(int argc, char *argv[])
 	//Call copy_from_to:
 	if (copy_from_to(source_file_name, destination_file_name))
 	{
-		printf("Content from the file %s was successfully copied to the file %s. \n",
+		printf("Content from the file '%s' was successfully copied to the file '%s'. \n",
 			source_file_name,
 			destination_file_name);
 	}
@@ -72,20 +82,150 @@ int main(int argc, char *argv[])
 
 //Definitions of the functions:
 bool copy_from_to(char source_file_name[], char destination_file_name[])
+/**
+ * Function name: copy_from_to
+ * Description: this function copies the content from the source file described
+ * by the filename 'source_file_name' to the destination file described by filename 
+ * 'destination_file_name'. If the destination file does not exist, it is created,
+ * otherwise the destination file's content is overwritten by the new content. If 
+ * the source file does not exist, no content is copied and no destination file 
+ * is created.
+ * 
+ * Input: (char []) source_file_name --> The name of the source file. May be a relative 
+ *                 name or an absolute name. If it is a relative name, the system 
+ *  			   will search for that file in the current directory.
+ *        (char []) destination_file_name --> The name of the destination file. May be a relative 
+ *                 name or an absolute name. If it is a relative name, the system 
+ *   			   will search for that file in the current directory.
+ *
+ * 
+ * Output: (bool) --> true if the content from file 'source_file_name' was 
+ * successfully copied to file 'destination_file_name'.
+ * 
+ */
 {
 	//---------------------------------------------------------------
 	//Declare Variables:
+	/*General*/
+	char function_name[] = "copy_from_to";
 	bool return_value = false;
-	////////Continue: should I use fopen or open??????????????????????????????????????????????
-	//Yes, I should use open (POSIX function).
-	FILE source_fd, destination_fd;
+	
+	/*Files*/
+	int src_fd = INVALID_FD, dest_fd = INVALID_FD;
+
+	/*Transfer information*/
+	ssize_t bytes_read, bytes_written;
+	size_t block_transfer_sz = 5 * MAX_BUFFER_SZ;
+	byte buffer[block_transfer_sz];
+	bool reached_EOF = false;
 	
 	
+	//---------------------------------------------------------------
+	//Open Source file:
+	src_fd = open(source_file_name, O_RDONLY);
+
+	if(src_fd == INVALID_FD)
+	{
+		handle_error(
+			function_name,
+			"Error while opening the source file.",
+			errno);
+		goto EXIT_COPY_FROM_TO;
+	}
+	else
+	{
+		printf("Source file successfully opened.\n");
+	}
+	
+	//---------------------------------------------------------------
+	//Create the Destination file:
+	dest_fd = creat(destination_file_name, S_IRWXU | S_IRWXG);
+
+	if(dest_fd == INVALID_FD)
+	{
+		handle_error(
+			function_name,
+			"Error while creating the destination file.",
+			errno);
+		goto EXIT_COPY_FROM_TO;
+	}
+	else
+	{
+		printf("Destination file successfully created.\n");
+	}
+	
+	//---------------------------------------------------------------
+	//Copy the content from source file to destination file using the
+	//buffer to transfer each block of memory:
+	
+	do{
+		//---------------------------------------------------------------
+		//Read the next block of memory from source file:
+		bytes_read = read(src_fd, buffer, block_transfer_sz);
+
+		if(bytes_read == 0)
+		{
+			reached_EOF = true;
+		}
+		else if (bytes_read == READ_FAILED)
+		{
+			handle_error(
+				function_name,
+				"Error while reading a block of memory from the source file.",
+				errno);
+			goto EXIT_COPY_FROM_TO;
+		}
+
+		//---------------------------------------------------------------
+		//Write the block of memory read from source file to destination
+		//file:
+		bytes_written = write(dest_fd, buffer, bytes_read);
+
+		if(bytes_written == WRITE_FAILED)
+		{
+			handle_error(
+				function_name,
+				"Error while writing a block of memory to the destination file.",
+				errno);
+			goto EXIT_COPY_FROM_TO;
+		}
+		else if (bytes_written != bytes_read)
+		{
+			handle_error(
+				function_name,
+				"Information lost. Difference in size between blocks of memory read and written.",
+				NON_UNIX_STD_ERROR_NO);
+			goto EXIT_COPY_FROM_TO;
+		}
+
+		//---------------------------------------------------------------
+	}while(!reached_EOF);
+	
+
+	//---------------------------------------------------------------
+	//Content from source file was successfully copied to destination 
+	//file:
+	return_value = true;	
+
+	//---------------------------------------------------------------
+	//Exit setup:
+EXIT_COPY_FROM_TO:
+	//Close open files:
+	if(dest_fd)
+		close(dest_fd);
+
+	if(src_fd)
+		close(src_fd);
+	
+	//Return the final result:
+	return return_value;
+
 	//---------------------------------------------------------------
 }
 
 void handle_error(char function_name[], char err_msg[], int err_no)
 /**
+ * Function name: handle_error
  * Description: This function handles the error that happened while executing the 
  * function with name 'function_name'. It prints a customized message and the 
  * standard UNIX error message if there is any. The 'err_msg' should not be finished 
