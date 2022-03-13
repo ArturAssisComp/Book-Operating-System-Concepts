833ad0a817c3ff6f3d29f09a6aef8e06968d1b8a
Artur Assis Alves <ArturAssisComp@gmail.com>
2022-03-12 18:05:05 -0700

    Solved project 3

--- Programming_Projects/cap3/project2/task_info.c
+++ Programming_Projects/cap3/project2/task_info.c
@@ -13,12 +13,13 @@
  */
 
 #include <linux/init.h>
+#include <linux/slab.h>
+#include <linux/sched.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
-//#include <linux/proc_fs.h>
-//#include <asm/uaccess.h>
-#include <linux/sched.h>
-#include <linux/uaccess.h>
+#include <linux/proc_fs.h>
+#include <linux/vmalloc.h>
+#include <asm/uaccess.h>
 
 #define BUFFER_SIZE 128
 
@@ -29,7 +30,7 @@ static long current_pid;
  * Function prototypes
  */
 static ssize_t proc_read(struct file *file, char *buf, size_t count, loff_t *pos);
-static ssize_t proc_write(struct file *file, char __user *usr_buf, size_t count, loff_t *pos);
+static ssize_t proc_write(struct file *file, const char __user *usr_buf, size_t count, loff_t *pos);
 
 static struct file_operations proc_ops = {
         .owner = THIS_MODULE,
@@ -45,7 +46,7 @@ static int proc_init(void)
         // creates the /proc/pid entry
         // the following function call is a wrapper for
         // proc_create_data() passing NULL as the last argument
-        proc_create(PROC_NAME, 0, NULL, &proc_ops);
+        proc_create(PROC_NAME, 0666, NULL, &proc_ops);
 
         printk(KERN_INFO "/proc/%s created\n", PROC_NAME);
 
@@ -55,7 +56,7 @@ static int proc_init(void)
 /* This function is called when the module is removed. */
 static void proc_exit(void) {
 
-        // removes the /proc/jiffies entry
+        // removes the /proc/pid entry
         remove_proc_entry(PROC_NAME, NULL);
 
         printk( KERN_INFO "/proc/%s removed\n", PROC_NAME);
@@ -75,7 +76,7 @@ static ssize_t proc_read(struct file *file, char __user *usr_buf, size_t count, 
         int rv = 0;
         char buffer[BUFFER_SIZE];
         static int completed = 0;
-        task_struct target_task_struct;
+        struct task_struct *target_task_struct = NULL;
 
         if (completed) {
                 completed = 0;
@@ -93,25 +94,18 @@ static ssize_t proc_read(struct file *file, char __user *usr_buf, size_t count, 
         }
 
         //Store target task struct info into the kernel buffer:
-        if(sprintf(buffer, "PID: %d\nSTATE: %u\nCommand: %s\n", target_task_struct->pid, target_task_struct->__state, target_task_struct->comm) < 0)
-        {
-            rv = -1;
-            goto end;
-        }
-        printk(KERN_INFO "%s\n", buffer);
+	rv = sprintf(buffer, "PID: %d\nSTATE: %ld\nCommand: %s\n", target_task_struct->pid, target_task_struct->state, target_task_struct->comm); 
+        if(rv < 0) goto end;
 
-        // copies the contents of buffer to userspace usr_buf
         if (copy_to_user(usr_buf, buffer, rv)) rv = -1;
 
 end:
         return rv;
 }
 
-static ssize_t proc_write(struct file *file, char __user *usr_buf, size_t count, loff_t *pos)
+static ssize_t proc_write(struct file *file, const char __user *usr_buf, size_t count, loff_t *pos)
 {
-    int rv = 0;
     char *k_mem;
-    const int base = 10;
 
     /*Allocate kernel memory*/
     k_mem = kmalloc(count, GFP_KERNEL);
@@ -119,9 +113,8 @@ static ssize_t proc_write(struct file *file, char __user *usr_buf, size_t count,
     /*Copies user space usr_buf to kernel memory */
     copy_from_user(k_mem, usr_buf, count);
 
-    printk(KERN_INFO "%s\n", k_mem);
-    //kstrtol(k_mem, base, &current_pid);
-    sscanf(k_mem, "%lu", &current_pid)
+    sscanf(k_mem, "%lu", &current_pid);
+    printk(KERN_INFO "Get pid: %lu\n", current_pid);
 
 
 
